50d207bf1be6ae081c190942b7296717394647b7
hn2 <hannan2@gmail.com>
2023-12-20 15:23:41 +0200

    Commit

--- Sql/Branches.sql
+++ Sql/Branches.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS z_target.Branches;
 CREATE TABLE z_target.Branches AS
 SELECT 	cast(siteid as unsigned) as 'id',
 		cast(Name as nchar) as 'Site Name'
-FROM z_source.sites
+FROM z_source.sites;
 
 select * from z_target.Branches;
